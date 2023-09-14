e9c2246eb1c3ad49ff461da2d9dbfe16312b76a1
HamzaLahmedi <hamzalahmedi07@gmail.com>
2023-09-14 15:48:04 +0100

    add custom button

--- lib/widgets/note_modal_bottum_sheet.dart
+++ lib/widgets/note_modal_bottum_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class ModalButtomSheet extends StatelessWidget {
@@ -8,22 +9,31 @@ class ModalButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 33,
-          ),
-          CustomTextField(
-            hint: 'title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'content',
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 33,
+            ),
+            CustomTextField(
+              hint: 'title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            CustomButton(),
+             SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
